c8e2bba25534e65ea8a9309d3146c161818178ab
EC2 Default User <ec2-user@ip-172-31-3-232.ap-northeast-1.compute.internal>
2022-09-12 10:17:09 +0000

    エラー修正

--- memo.rb
+++ memo.rb
@@ -2,12 +2,12 @@ require "csv" # CSVファイルを扱うためのライブラリを読み込ん�
  
  puts "1 → 新規でメモを作成する / 2 → 既存のメモを編集する"
  
- memo_type = gets.to_i # ユーザーの入力値を取得し、数字へ変換しています
+ memo_type = gets.to_s 
  input_number = memo_type.chomp
  
  # if文を使用して続きを作成していきましょう。
  # 「memo_type」の値（1 or 2）によって処理を分岐させていきましょう。
- if input_number == 1
+ if input_number == "1"
  puts "新規でメモを作成します。拡張子を除いたファイル名を入力してください。"
  file_name = gets.chomp
   
@@ -19,7 +19,7 @@ require "csv" # CSVファイルを扱うためのライブラリを読み込ん�
  csv.puts ["#{memo}"]
  end
  
- elsif input_number == 2
+ elsif input_number == "2"
  puts "既存のメモを編集します。拡張子を除いた既存ファイル名を入力してください。"
  
  file_name = gets.chomp
